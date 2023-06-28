157e17901ea4e8e1c61b81d564a1428b83a5fddf
Blaircart16 <harlesmyler@gmail.com>
2023-06-28 18:09:38 +0300

    edit 1

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>HTML Codex - Login Form Template</title>
+        <title>Emosense</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="Login Form Template" name="keywords">
         <meta content="Login Form Template" name="description">
